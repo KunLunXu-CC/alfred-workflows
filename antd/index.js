a8a999d3c578537d2be16ba1107df8c58204b101
墨渊君 <moyuanjun925@gmail.com>
2023-02-06 16:57:22 +0800

    chore: update antd

--- antd/index.js
+++ antd/index.js
@@ -9,16 +9,17 @@ const config = JSON.parse(
   fs.readFileSync('./config.json', 'utf-8')
 )[versions]
 
-// 3. 过滤或者选择项
+// 2. 根据「检索参数」进行过滤
 const filterData = config.filter(
   v => v.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())
 );
 
-// 4. 处理过滤后数据
+// 3. 处理过滤后数据: 过滤后数据为空, 则返回全部、追加额外字段 subtitle
 const items = (filterData.length !== 0 ? filterData : config)
 .map(v => ({
   ...v,
   subtitle: `打开组件: ${v.title}, 按住 cmd 打开 API`,
+  // mod 可以设置通过控制不同按键, 来修改覆盖默认参数
   mods: {
     cmd: {
       arg: `${v.arg}#api`,
@@ -27,5 +28,5 @@ const items = (filterData.length !== 0 ? filterData : config)
   },
 }))
 
-// 5. 输出
+// 5. 输出下拉项
 alfy.output(items);
